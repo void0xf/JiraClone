a170d48b4fe783ac14bd73f3384314e22d83e363
unknown <lucyfer99100@gmail.com>
2025-03-08 00:18:38 +0100

    feat: implement CQRS for project create/read

---
+++ backend/Services/ProjectService.API/Features/CreateProject/CreateProjectHandler.cs
@@ -0,0 +1,32 @@
+﻿using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.CreateProject;
+
+public record CreateProjectCommand(
+    string Name, 
+    string ProjectKey, 
+    AccessLevel AccessLevel,
+    ProjectTemplate  ProjectTemplate) : IRequest<CreateProjectResult>;
+
+public record CreateProjectResult(Guid ProjectId);
+
+public class CreateProjectHandler : IRequestHandler<CreateProjectCommand, CreateProjectResult>
+{
+    public async Task<CreateProjectResult> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+    {
+        var project = new Project
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            ProjectKey = request.ProjectKey,
+            AccessLevel = request.AccessLevel,
+            ProjectTemplate = request.ProjectTemplate,
+            CreatedAt = DateTime.UtcNow,
+            LeadId = Guid.Empty,
+            Members = new List<Guid>(),
+            UpdatedAt = DateTime.UtcNow
+        };
+        //save in db
+        return new CreateProjectResult(project.Id);
+    }
+}
---
+++ backend/Services/ProjectService.API/Features/CreateProject/GetProjectsByLeadQuery.cs
@@ -0,0 +1,17 @@
+﻿using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.CreateProject;
+
+public record GetProjectsByLeadQuery(Guid leadId) : IRequest<GetProjectsByLeadQueryResult>;
+
+public record GetProjectsByLeadQueryResult(List<Project> Projects);
+
+
+public class GetProjectsByLeadHandler : IRequestHandler<GetProjectsByLeadQuery, GetProjectsByLeadQueryResult>
+{
+    public async Task<GetProjectsByLeadQueryResult> Handle(GetProjectsByLeadQuery request, CancellationToken cancellationToken)
+    {
+        //query db
+        return new GetProjectsByLeadQueryResult(new List<Project>());
+    }
+}
