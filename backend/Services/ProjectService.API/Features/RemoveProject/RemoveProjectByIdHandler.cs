965db5e29eb337c5b68b4299e8849398aa1b436c
unknown <lucyfer99100@gmail.com>
2025-03-12 15:11:03 +0100

    feat(ProjectService): add remove operation for projects

---
+++ backend/Services/ProjectService.API/Features/RemoveProject/RemoveProjectByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using ProjectService.API.Features.CreateProject;
+using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.RemoveProject;
+
+public record RemoveProjectCommand(Guid ProjectId) : IRequest<ReemoveProjectResult>; 
+
+public record ReemoveProjectResult(Guid ProjectId);
+
+public class RemoveProjectByIdHandler(IDocumentSession session) : IRequestHandler<RemoveProjectCommand, ReemoveProjectResult>
+{
+    public async Task<ReemoveProjectResult> Handle(RemoveProjectCommand request, CancellationToken cancellationToken)
+    {
+        var projectToDelete = await session.Query<Project>()
+            .FirstOrDefaultAsync(p => p.Id == request.ProjectId, cancellationToken);
+        if (projectToDelete != null)
+        {
+            session.Delete<Project>(projectToDelete);
+            await session.SaveChangesAsync(cancellationToken);
+            return new ReemoveProjectResult(projectToDelete.Id);
+        }
+        return new ReemoveProjectResult(Guid.Empty);
+    }
+}
