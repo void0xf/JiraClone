565406d14eaa4529e1c7136d52a2355ffa6607d9
unknown <lucyfer99100@gmail.com>
2025-03-12 15:10:33 +0100

    refactor(ProjectService): moved GetProjectsByLeadId operation

---
+++ backend/Services/ProjectService.API/Features/GetProject/GetProjectsByLeadIdEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.GetProject;
+
+public record GetProjectsByLeadIdQueryResponse(List<Project> Projects);
+
+
+public class GetProjectsByLeadIdEndpoint: ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/projects", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProjectsByLeadIdQuery());
+            return result.Adapt<GetProjectsByLeadIdQueryResponse>();
+        });
+    }
+}
