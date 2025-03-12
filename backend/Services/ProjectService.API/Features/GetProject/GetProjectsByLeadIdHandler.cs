218979ee4fda5a5f07d2242b939322280bf3dcbf
unknown <lucyfer99100@gmail.com>
2025-03-12 15:10:33 +0100

    refactor(ProjectService): moved GetProjectsByLeadId operation

---
+++ backend/Services/ProjectService.API/Features/GetProject/GetProjectsByLeadIdHandler.cs
@@ -0,0 +1,19 @@
+﻿using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.GetProject;
+
+public record GetProjectsByLeadIdQuery() : IRequest<GetProjectsByLeadIdQueryResult>;
+
+public record GetProjectsByLeadIdQueryResult(List<Project> Projects);
+
+
+public class GetProjectsByLeadIdHandler(IDocumentSession session) : IRequestHandler<GetProjectsByLeadIdQuery, GetProjectsByLeadIdQueryResult>
+{
+    public async Task<GetProjectsByLeadIdQueryResult> Handle(GetProjectsByLeadIdQuery request, CancellationToken cancellationToken)
+    {
+        var projects = await session.Query<Project>()
+            //.Where(p => p.LeadId == request.LeadId) auth needed for that
+            .ToListAsync(cancellationToken);   
+        return new GetProjectsByLeadIdQueryResult(projects.ToList());
+    }
+}
