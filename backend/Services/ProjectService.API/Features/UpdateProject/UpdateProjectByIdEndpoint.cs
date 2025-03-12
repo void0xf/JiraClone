2e8a27c2ddc4a236867676e714f639284bd0f4d4
unknown <lucyfer99100@gmail.com>
2025-03-12 15:11:15 +0100

    feat(ProjectService): add update operation for projects

---
+++ backend/Services/ProjectService.API/Features/UpdateProject/UpdateProjectByIdEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using ProjectService.API.Features.CreateProject.DTO;
+using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.UpdateProject;
+
+public class UpdateProjectByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/projects/{id}", async (HttpContext context, ISender sender, Guid id) =>
+        {
+            var bodyData = await context.Request.ReadFromJsonAsync<ProjectRequestDto>();
+            if (bodyData == null) return Guid.Empty;
+
+            var project = bodyData.Adapt<Project>();
+            project.Id = id;
+            var responese = await sender.Send(new UpdateProjectCommand(project));
+            
+            return responese.projectId;
+        });
+    }
+}
