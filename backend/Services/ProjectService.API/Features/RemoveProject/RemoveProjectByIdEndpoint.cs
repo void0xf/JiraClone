2154919b8f541cfe635273f0ebfc742cc57e87bb
unknown <lucyfer99100@gmail.com>
2025-03-12 15:11:03 +0100

    feat(ProjectService): add remove operation for projects

---
+++ backend/Services/ProjectService.API/Features/RemoveProject/RemoveProjectByIdEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectService.API.Features.RemoveProject;
+
+public class RemoveProjectByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("projects/{id}", async (ISender sender, Guid id) =>
+        {
+            var response = await sender.Send(new RemoveProjectCommand(id));
+            return response;
+        });
+    }
+}
