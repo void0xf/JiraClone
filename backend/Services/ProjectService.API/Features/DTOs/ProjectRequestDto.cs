9d38a7c29027e1ccac4cd63c72b2556efe78114b
unknown <lucyfer99100@gmail.com>
2025-03-12 15:09:34 +0100

    feat(ProjectService): add create operation for projects

---
+++ backend/Services/ProjectService.API/Features/DTOs/ProjectRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using ProjectService.API.Models;
+
+namespace ProjectService.API.Features.CreateProject.DTO;
+
+public class ProjectRequestDto
+{ 
+        public string Name { get; set; } = string.Empty;
+        public ProjectTemplate ProjectTemplate { get; set; }
+        public string ProjectKey { get; set; } = string.Empty;
+        public AccessLevel AccessLevel { get; set; }
+}
