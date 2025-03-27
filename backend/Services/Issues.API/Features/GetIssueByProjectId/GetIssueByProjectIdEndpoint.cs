d55587d612c5290998eaaebf7c3290aaa75efc2f
unknown <lucyfer99100@gmail.com>
2025-03-28 00:19:30 +0100

    feat(IssueService): implement GetIssueByProjectId feature

---
+++ backend/Services/Issues.API/Features/GetIssueByProjectId/GetIssueByProjectIdEndpoint.cs
@@ -0,0 +1,56 @@
+﻿using Carter;
+using Issues.API.Features.GetIssueByProjectId.DTO;
+using MediatR;
+using SharedKernel;
+
+namespace Issues.API.Features.GetIssueByProjectId
+{
+    public class GetIssueByProjectIdEndpoint : CarterModule
+    {
+        public GetIssueByProjectIdEndpoint() : base("api/v1") { }
+
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/issue/project/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetIssueByProjectIdQuery(id));
+
+                if (result.IsSuccess)
+                {
+                    var issuesDto = result.Value.Issues.Select(issue => new IssueDto(
+                        issue.Id,
+                        issue.Key,
+                        issue.ProjectId,
+                        issue.IssueType,
+                        issue.ParentIssueId,
+                        issue.Summary,
+                        issue.Description,
+                        issue.Status,
+                        issue.Priority,
+                        issue.AssigneeId,
+                        issue.ReporterId,
+                        issue.SprintId,
+                        issue.Labels.Select(l => new LabelDto(l.Id, l.Name)).ToList(),
+                        issue.EstimatedStoryPoints,
+                        issue.IsArchived,
+                        issue.InBacklog,
+                        issue.DueDate,
+                        issue.CreatedAt,
+                        issue.UpdatedAt
+                    )).ToList();
+
+                    return ApiResponse<GetIssueByProjectIdResponse>.
+                        Success(new GetIssueByProjectIdResponse(issuesDto)).ToMinimalApiResult();
+                }
+
+                return result.ToApiResponse().ToMinimalApiResult();
+
+               
+            })
+            .WithName("GetIssuesByProjectId") // Name for the endpoint
+            .Produces<ApiResponse<GetIssueByProjectIdResponse>>(StatusCodes.Status200OK) // Successful response type
+            .ProducesProblem(StatusCodes.Status404NotFound) // Error response type
+            .WithSummary("Get Issues By Project ID"); // Summary for the endpoint
+        }
+    }
+}
---
+++ backend/Services/Issues.API/Features/GetIssueByProjectId/GetIssueByProjectIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using Issues.API.Data;
+using Issues.API.Models;
+using MediatR;
+using SharedKernel;
+
+namespace Issues.API.Features.GetIssueByProjectId;
+
+public record GetIssueByProjectIdQuery(Guid projectId) : IRequest<Result<GetIssueByProjectIdResult>>;
+public record GetIssueByProjectIdResult(List<Issue> Issues);
+    
+public class GetIssueByProjectIdHandler(IIssueRepository repository) : IRequestHandler<GetIssueByProjectIdQuery, Result<GetIssueByProjectIdResult>>
+{
+    public async Task<Result<GetIssueByProjectIdResult>> Handle(GetIssueByProjectIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await repository.GetIssueByProjectId(request.projectId, cancellationToken);
+        if (result.IsFailure)
+        {
+            return Result<GetIssueByProjectIdResult>.Failure(result.Error);
+        }
+        return Result<GetIssueByProjectIdResult>.Success(new GetIssueByProjectIdResult(result.Value));
+    }
+}
