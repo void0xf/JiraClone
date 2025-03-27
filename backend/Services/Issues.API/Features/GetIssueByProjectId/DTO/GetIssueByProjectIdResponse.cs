9d2e4782c53e1b01f6535d9947df6f26658abcaa
unknown <lucyfer99100@gmail.com>
2025-03-28 00:19:30 +0100

    feat(IssueService): implement GetIssueByProjectId feature

---
+++ backend/Services/Issues.API/Features/GetIssueByProjectId/DTO/GetIssueByProjectIdResponse.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+using Issues.API.Models;
+
+namespace Issues.API.Features.GetIssueByProjectId.DTO;
+
+public record GetIssueByProjectIdRequest(Guid projectId);
+public record GetIssueByProjectIdResponse(List<IssueDto> Issues);
+
+public record IssueDto(
+    Guid Id,
+    string Key,
+    Guid ProjectId,
+    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    IssueType IssueType,
+    string? ParentIssueId,
+    string Summary,
+    string Description,
+    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    IssueStatus Status,
+    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    IssuePriority Priority,
+    string? AssigneeId,
+    string ReporterId,
+    string? SprintId,
+    List<LabelDto> Labels,
+    int? EstimatedStoryPoints,
+    bool IsArchived,
+    bool InBacklog,
+    DateTime? DueDate,
+    DateTime CreatedAt,
+    DateTime UpdatedAt);
+
+public record LabelDto(Guid Id, string Name); 
