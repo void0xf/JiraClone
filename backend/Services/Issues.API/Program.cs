4d0d331802900c8911a4b9e826442a4ba3fba4dc
unknown <lucyfer99100@gmail.com>
2025-03-28 00:08:37 +0100

    chore(db): set up database for microservice

--- backend/Services/Issues.API/Program.cs
+++ backend/Services/Issues.API/Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
+}).UseLightweightSessions();
+
 
 app.Run();
