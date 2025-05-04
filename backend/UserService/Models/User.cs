d5529b627c446e2d18f81d1d92ca569dcffb1559
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-04 16:36:08 +0200

    feat(UserService): inital User model

---
+++ backend/UserService/Models/User.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Models
+{
+    public enum PrivacyLevel
+    {
+        Public,
+        Private,
+    }
+
+    public class PrivacySetting
+    {
+        public string? Value { get; set; }
+        public PrivacyLevel WhoCanSee { get; set; } = PrivacyLevel.Private;
+    }
+    
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(255)]
+        public string KeycloakUserId { get; set; } = null!;
+        public PrivacySetting? FullName { get; set; }
+        public PrivacySetting? PublicName { get; set; }
+        public string HeaderImageUrl { get; set; } = string.Empty;
+        public string ProfilePictureUrl { get; set; } = string.Empty;   
+        public List<Guid> WorkedOn { get; set; } = new();
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
