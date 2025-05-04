ada8cd73f53216970e3df8777d83b66854f45142
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-04 16:37:15 +0200

    feat(UserService): EF migration for user model

---
+++ backend/UserService/Migrations/20250504111959_inital-create.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    KeycloakUserId = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    FullNameValue = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    FullNamePrivacy = table.Column<int>(type: "integer", nullable: true),
+                    PublicNameValue = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    PublicNamePrivacy = table.Column<int>(type: "integer", nullable: true),
+                    HeaderImageUrl = table.Column<string>(type: "text", nullable: false),
+                    ProfilePictureUrl = table.Column<string>(type: "text", nullable: false),
+                    WorkedOn = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_KeycloakUserId",
+                table: "Users",
+                column: "KeycloakUserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
