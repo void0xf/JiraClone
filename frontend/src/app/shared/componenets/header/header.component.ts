72afa62a8815a53df036eb1cb02eb050812be6a8
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-11 18:33:42 +0200

    feat(header): add header component (#17)

---
+++ frontend/src/app/shared/componenets/header/header.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+// import { HlmSpinnerComponent } from-ng/ui-spinner-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  lucidePanelLeftClose,
+  lucideLayoutGrid,
+  lucidePlus,
+  lucideCreditCard,
+  lucideBell,
+  lucideCircleHelp,
+  lucideSettings,
+  lucideSearch,
+  lucideEllipsis,
+  lucidePanelLeftOpen,
+  lucidePanelRightOpen,
+} from '@ng-icons/lucide';
+import { SearchComponent } from '../search/search.component';
+import { SidebarService } from '../../../core/services/sidebar.service';
+import {
+  HlmTooltipComponent,
+  HlmTooltipTriggerDirective,
+} from '@spartan-ng/ui-tooltip-helm';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    HlmButtonDirective,
+    HlmIconDirective,
+    NgIcon,
+    SearchComponent,
+    HlmTooltipComponent,
+    HlmTooltipTriggerDirective,
+    HlmButtonDirective,
+  ],
+  providers: [
+    provideIcons({
+      lucidePanelLeftClose,
+      lucidePanelLeftOpen,
+      lucidePanelRightOpen,
+      lucideLayoutGrid,
+      lucidePlus,
+      lucideCreditCard,
+      lucideBell,
+      lucideCircleHelp,
+      lucideSettings,
+      lucideSearch,
+      lucideEllipsis,
+    }),
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  constructor(private sidebarService: SidebarService) {}
+
+  get isSidebarOpen() {
+    return this.sidebarService.isOpen;
+  }
+
+  toggleSidebar(): void {
+    this.sidebarService.toggle();
+  }
+}
