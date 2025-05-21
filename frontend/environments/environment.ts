9af4dae597fbdb2973baa8769ecf9ec0f501ac45
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-21 19:45:44 +0200

    feat(environment): add environment configuration for development with Keycloak settings

---
+++ frontend/environments/environment.ts
@@ -0,0 +1,9 @@
+export const envirovment = {
+  production: false,
+  projectApiBaseUrl: 'http://localhost:5000',
+  keycloakSettings: {
+    url: 'http://localhost:8080',
+    realm: 'jira-clone',
+    clientId: 'jira-clone-frontend',
+  },
+};
