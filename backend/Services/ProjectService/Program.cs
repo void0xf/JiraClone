aac20a839c5ee023f0399c96169ec7fbbb4a7e8c
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-21 19:51:44 +0200

    feat(ProjectService): integrate CORS policy

--- backend/Services/ProjectService/Program.cs
+++ backend/Services/ProjectService/Program.cs
@@ -1,28 +1,27 @@
 using System.Security.Claims;
+using Carter; // Assuming you have this using statement for AddCarter/MapCarter
+using Marten; // Assuming you have this using statement for AddMarten
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var keycloakConfig = builder.Configuration.GetSection("Keycloak");
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-/*builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();*/
-builder.Services.AddHttpContextAccessor(); 
+// Define your CORS policy name
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+// Add services to the container.
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
-
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
 }).UseLightweightSessions();
-
 
 builder.Services.AddAuthentication(options =>
     {
@@ -41,26 +40,35 @@ builder.Services.AddAuthentication(options =>
             ValidateAudience = true,
             ValidAudience = keycloakConfig["Audience"],
             ValidateLifetime = true,
-            NameClaimType = ClaimTypes.Name, 
-            RoleClaimType = ClaimTypes.Role 
+            NameClaimType = ClaimTypes.Name,
+            RoleClaimType = ClaimTypes.Role
         };
     });
 
 builder.Services.AddAuthorization();
 
-var app = builder.Build();
-app.MapCarter();
-// Configure the HTTP request pipeline.
-/*
-if (app.Environment.IsDevelopment())
+builder.Services.AddCors(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
 
-app.UseHttpsRedirection();
-*/
-app.UseAuthentication(); 
-app.UseAuthorization(); 
+
+var app = builder.Build();
+
+
+app.UseCors(MyAllowSpecificOrigins);
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapCarter(); // Carter endpoints will now be subject to the CORS policy
+
 app.Run();
-
