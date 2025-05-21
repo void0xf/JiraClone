628cf20d9c41c080fb98ea12a3890320435b4122
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-21 19:56:16 +0200

    feat(frontend): integrate keycloak configuration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideChevronRight } from '@ng-icons/lucide';
+import Keycloak from 'keycloak-js';
+import { KeycloakStatusService } from './core/services/keycloak.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,5 +13,10 @@ import { lucideChevronRight } from '@ng-icons/lucide';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private keycloakStatusService = inject(KeycloakStatusService);
+
+  keycloakStatus = this.keycloakStatusService.keycloakType;
+  authenticated = this.keycloakStatusService.authenticated;
+
   title = 'frontend';
 }
