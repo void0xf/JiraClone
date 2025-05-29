8f5795f41b898bee5baa2c7279474ed849579293
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-29 16:07:23 +0200

    refactor(project-routing): replace ProjectRoutingModule with project.routes.ts

---
+++ frontend/src/app/core/layout/project-layout/project.routes.ts
@@ -0,0 +1,40 @@
+import { Routes } from '@angular/router';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { ProjectLayoutComponent } from './project-layout.component';
+import { ProjectSummaryComponent } from '../../../features/project-managment/project-summary/project-summary.component';
+import {
+  projectFeatureKey,
+  projectReducer,
+} from '../../../features/project-managment/store/project.reducer';
+import { ProjectEffects } from '../../../features/project-managment/store/project.effects';
+import { ProjectBoardComponent } from '../../../features/project-managment/project-board/project-board.component';
+
+export const PROJECT_ROUTES: Routes = [
+  {
+    path: ':project_key', // This is relative to the path where these routes are loaded
+    component: ProjectLayoutComponent,
+    providers: [
+      provideState(projectFeatureKey, projectReducer),
+      provideEffects([ProjectEffects]),
+    ],
+    children: [
+      { path: '', redirectTo: 'summary', pathMatch: 'full' },
+      {
+        path: 'summary',
+        component: ProjectSummaryComponent,
+        title: 'Project Summary',
+      },
+      {
+        path: 'timeline',
+        component: ProjectSummaryComponent,
+        title: 'Project Timeline',
+      },
+      {
+        path: 'board',
+        component: ProjectBoardComponent,
+        title: 'Project Board',
+      },
+    ],
+  },
+];
