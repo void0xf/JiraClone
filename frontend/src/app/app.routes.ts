91af088b82e3414b23a25afec05e6e18a702a5cd
void0xf <150953141+void0xf@users.noreply.github.com>
2025-05-29 16:12:16 +0200

    feat(routes): update to project.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './features/auth/sign-in/sign-in.component';
+import { ForYouComponent } from './features/for-you/for-you.component';
 
 export const routes: Routes = [
   {
@@ -7,9 +8,13 @@ export const routes: Routes = [
     path: 'jira/software/projects',
     // Lazy load the module responsible for handling project-related views
     loadChildren: () =>
-      import('./core/layout/project-layout/project-routing.module').then(
-        (m) => m.ProjectRoutingModule
+      import('./core/layout/project-layout/project.routes').then(
+        (m) => m.PROJECT_ROUTES
       ),
+  },
+  {
+    path: 'jira/your-work',
+    component: ForYouComponent,
   },
   {
     path: 'jira/software/sign-in',
