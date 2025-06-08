52f332411a42a0c9cf301badf28c4c58152c9d98
void0xf <150953141+void0xf@users.noreply.github.com>
2025-06-08 19:32:24 +0200

    feat(IssueService): add CORS policy to allow requests from localhost:4200

--- backend/Services/IssueService/Program.cs
+++ backend/Services/IssueService/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.IdentityModel.Tokens;
 var assembly = typeof(Program).Assembly;
 var builder = WebApplication.CreateBuilder(args);
 var keycloakConfig = builder.Configuration.GetSection("Keycloak");
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddScoped<IIssueRepository, IssueRepository>();
 builder.Services.AddHttpContextAccessor(); 
@@ -49,9 +50,22 @@ builder.Services.AddAuthentication(options =>
         };
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthentication();
 app.UseAuthorization();
