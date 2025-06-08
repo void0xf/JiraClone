9f786e0717db1decb9054eb7e91300abbb9860ae
void0xf <150953141+void0xf@users.noreply.github.com>
2025-06-08 19:31:58 +0200

    feat(environment): add issue API base URL to environment configuration

--- frontend/environments/environment.ts
+++ frontend/environments/environment.ts
@@ -1,9 +1,10 @@
 export const envirovment = {
   production: false,
   projectApiBaseUrl: 'http://localhost:5000',
+  issueApiBaseUrl: 'http://localhost:5001/api/v1',
   keycloakSettings: {
     url: 'http://localhost:8080',
     realm: 'jira-clone',
-    clientId: 'jira-clone-frontend',
+    clientId: 'jira-clone-frontend'
   },
 };
