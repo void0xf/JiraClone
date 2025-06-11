acc1e4df5c4a4c10666075236ecd745937116586
void0xf <150953141+void0xf@users.noreply.github.com>
2025-06-11 23:56:40 +0200

    feat(project-board-column): implement new column item & text area

--- frontend/src/app/features/project-managment/board/project-board-column/project-board-column.component.ts
+++ frontend/src/app/features/project-managment/board/project-board-column/project-board-column.component.ts
@@ -2,13 +2,15 @@ import { Component, Input, ViewChild, OnInit, Output, EventEmitter, OnChanges, S
 import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem, CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
 import { ProjectBoardColumnItemComponent } from "../project-board-column-item/project-board-column-item.component";
 import { CommonModule, NgIf } from '@angular/common';
-import { Issue, IssueType, IssueStatus, IssuePriority } from '../../../../models/issue.model'; // Adjusted import path
+import { Issue, IssueType, IssueStatus, IssuePriority } from '../../../../models/issue.model';
+import { ProjectBoardNewItemButtonComponent } from '../project-board-new-item-button/project-board-new-item-button.component';
+import { ProjectBoardNewItemTextAreaComponent } from '../project-board-new-item-text-area/project-board-new-item-text-area.component';
 
 // Define an interface for the item data - REMOVED BoardItem
 
 @Component({
   selector: 'app-project-board-column',
-  imports: [CommonModule, CdkDropList, CdkDrag, ProjectBoardColumnItemComponent, NgIf, DragDropModule],
+  imports: [CommonModule, CdkDropList, CdkDrag, ProjectBoardColumnItemComponent, NgIf, DragDropModule, ProjectBoardNewItemButtonComponent, ProjectBoardNewItemTextAreaComponent],
   templateUrl: './project-board-column.component.html',
   styleUrl: './project-board-column.component.scss',
 })
@@ -17,10 +19,18 @@ export class ProjectBoardColumnComponent implements OnInit, DoCheck {
   @Input() items: Issue[] = [];
   @Input() isDoneColumn: boolean = false;
   @Output() itemDropped = new EventEmitter<CdkDragDrop<Issue[]>>();
+  isHovering = false;
+  newItemButtonClicked = false;
 
   itemCount: number = 0;
 
   constructor() {
+  }
+  newItemButtonClick() {
+    this.newItemButtonClicked = true;
+  }
+  focusLost() {
+    this.newItemButtonClicked = false;
   }
 
   ngOnInit(): void {
@@ -30,6 +40,13 @@ export class ProjectBoardColumnComponent implements OnInit, DoCheck {
    this.updateItemCount();
   }
 
+  onMouseEnter() {
+    console.log('hovering')
+    this.isHovering = true;
+  }
+  onMouseLeave() {
+    this.isHovering = false;
+  }
 
   updateItemCount(): void {
     this.itemCount = this.items.length;
