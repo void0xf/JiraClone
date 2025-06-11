1a50e3c47dcaf08ceff15cc8ce75b292404b2cfd
void0xf <150953141+void0xf@users.noreply.github.com>
2025-06-11 23:57:23 +0200

    feat(for-you): create ForYou component template

---
+++ frontend/src/app/features/for-you/for-you.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-you',
+  imports: [],
+  templateUrl: './for-you.component.html',
+  styleUrl: './for-you.component.scss'
+})
+export class ForYouComponent {
+
+}
