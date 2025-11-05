f44b116038e7c32bb2cf9697f0eb50f803f1ca06
void0xf <150953141+void0xf@users.noreply.github.com>
2025-11-05 13:56:29 +0100

    feat(environment): add userApiBaseUrl to environment configuration

--- frontend/environments/environment.ts
+++ frontend/environments/environment.ts
@@ -2,6 +2,7 @@ export const envirovment = {
   production: false,
   projectApiBaseUrl: 'http://localhost:5000',
   issueApiBaseUrl: 'http://localhost:5001/api/v1',
+  userApiBaseUrl: 'http://localhost:5002',
   keycloakSettings: {
     url: 'http://localhost:8080',
     realm: 'jira-clone',
