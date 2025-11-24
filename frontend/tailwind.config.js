b90cf43cf19fbf076c17ffc2fdbf656378a054cc
void0xf <150953141+void0xf@users.noreply.github.com>
2025-11-24 17:10:23 +0100

    feat: design tokens for styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,66 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  presets: [require("@spartan-ng/brain/hlm-tailwind-preset")],
-  content: ["./src/**/*.{html,ts}", "./libs/ui/**/*.{html,ts}"],
 
+// Helper to allow opacity modifiers: bg-primary/50
+function hsl(variableName) {
+  return `hsl(var(${variableName}) / <alpha-value>)`;
+}
+
+module.exports = {
+  presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
+  content: ['./src/**/*.{html,ts}', './libs/ui/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        "board-column-background": "var(--board-column-background)",
-        "board-column-card-background": "var(--board-column-card-background)",
-        "dialog-background" :"var(--dialog-background)",
-        "drop-down-background": "var(--drop-down-background)",
-        "dialog-input-background": "var(--dialog-input-background)",
-        "drop-down-selected-background": "var(--drop-down-selected-background)"
+        background: {
+          DEFAULT: hsl('--background'),
+          hover: hsl('--background-hover'),
+          active: hsl('--background-active'),
+        },
+        foreground: {
+          DEFAULT: hsl('--foreground'),
+          hover: hsl('--foreground-hover'),
+          active: hsl('--foreground-active'),
+        },
+        card: {
+          DEFAULT: hsl('--card'),
+          foreground: hsl('--card-foreground'),
+        },
+        popover: {
+          DEFAULT: hsl('--popover'),
+          foreground: hsl('--popover-foreground'),
+        },
+        primary: {
+          DEFAULT: hsl('--primary'),
+          foreground: hsl('--primary-foreground'),
+        },
+        secondary: {
+          DEFAULT: hsl('--secondary'),
+          foreground: hsl('--secondary-foreground'),
+        },
+        muted: {
+          DEFAULT: hsl('--muted'),
+          foreground: hsl('--muted-foreground'),
+        },
+        accent: {
+          DEFAULT: hsl('--accent'),
+          foreground: hsl('--accent-foreground'),
+        },
+        destructive: {
+          DEFAULT: hsl('--destructive'),
+          foreground: hsl('--destructive-foreground'),
+        },
+        border: hsl('--border'),
+        input: hsl('--input'),
+        ring: hsl('--ring'),
+
+        // Custom Jira Colors
+        board: {
+          col: hsl('--board-col-bg'),
+          card: hsl('--board-card-bg'),
+        },
+        dialog: {
+          DEFAULT: hsl('--dialog-bg'),
+        },
       },
     },
   },
